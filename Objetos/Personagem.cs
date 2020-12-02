8fa9598386904b745e82b6ecdb25999b62f804fc
matheus <matheusviniciusdavid@gmail.com>
2020-12-02 07:15:33 -0300

    Personagem

---
+++ Objetos/Personagem.cs
@@ -0,0 +1,19 @@
+namespace Nova_pasta2
+{
+    public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+
+
+
+        public string Atacar(){
+            return "Ataque!";
+        }
+
+        public string Defender(){
+            return "Nao foi dessa vez!";
+        }
+    }
+}
---
+++ Objetos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Nova_pasta2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem personagem1 = new Personagem();
+
+            personagem1.nome = "Matheus David";
+            personagem1.idade = 18;
+            personagem1.armadura = "Armadura de fogo";
+
+            Personagem personagem2 = new Personagem();
+
+            personagem2.nome = "Pereira";
+            personagem2.idade = 20;
+            personagem2.armadura = "Voadora";
+
+            Console.WriteLine($"Personagem 1 ={personagem1.nome} - {personagem1.Atacar()}");
+
+            Console.WriteLine($"Personagem 1 ={personagem2.nome} - {personagem2.Defender()}");
+        }
+    }
+}
